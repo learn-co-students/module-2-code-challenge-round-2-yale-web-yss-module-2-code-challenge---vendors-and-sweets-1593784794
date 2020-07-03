71b38246b6093caea6f15335c2fe5fc14da7b814
CelestialEmperor <mapeiyao@gmail.com>
2020-07-03 17:01:34 +0100

    Done w/bonus.

---
+++ app/models/vendor_sweet.rb
@@ -0,0 +1,8 @@
+class VendorSweet < ApplicationRecord
+    belongs_to :vendor
+    belongs_to :sweet
+    
+    validates :price, presence: true
+    validates :price, numericality: { greater_than: 0}
+    validates :vendor_id, :uniqueness => { :scope => :sweet_id }
+end
