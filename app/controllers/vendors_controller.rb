a8f5673f12bd5a760d787e5c6bc4277fbd8175bb
Jonkavaliauskas <jonas.kavaliauskas@yale.edu>
2020-07-03 19:00:36 +0300

    Done

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -2,6 +2,11 @@ class VendorsController < ApplicationController
 
   def index
     @vendors = Vendor.all
+    @avg_price = @vendors.avg_sweet_price
+  end
+
+  def show
+    @vendor = Vendor.find(params[:id])
   end
 
 end
